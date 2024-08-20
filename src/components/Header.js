34695fb6940bc044d933f56aa52a5cd787a10ae4
Cuma KAPLAN <ccumakaplan@gmail.com>
2024-08-20 14:32:31 +0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div style={{backgroundColor:"#5d3ebc"}} className='h-11  '>
+      <div className="container mx-auto">
+header 
+      </div>
+      
+    </div>
+  )
+}
