62281bbceed552d21b7659c93d1e71a11d6fd2ff
Cuma KAPLAN <ccumakaplan@gmail.com>
2024-08-21 21:56:52 +0300

    I did responsive

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,24 +1,26 @@
-import React, { useEffect, useState } from "react";
-import categoryData from "../api/categories.json";
-import Category from "./ui/Category.jsx";
+
+import { useState, useEffect } from 'react';
+import categoryData from 'api/categories.json'
+import Category from 'components/ui/Category'
 
 export default function Categories() {
-  const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
-    setCategories(categoryData);
-  }, []);
-  
-  return (
-    <div className="container mx-auto">
-      <div className="bg-white py-4">
-        <div className="container mx-auto">
-          <h3 className="font-semibold text-sm mb-3">Kategoriler</h3>
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+       
+        setCategories(categoryData)
+    }, [])
+
+    return (
+        <div className="bg-white py-4">
+            <div className="container mx-auto">
+                <h3 className="font-semibold text-sm mb-3 ">Kategoriler</h3>
+                <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 2xl:grid-cols-12">
+                   
+                    {categories && categories.map((category, index) => <Category key={index} category={category} />)}
+                </div>
+            </div>
         </div>
-        <div className="grid lg:grid-cols-10">
-        {categories && categories.map((category, index) => <Category key={index} category={category} />)}
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -67,24 +67,23 @@ export default function Footer() {
   ];
 
   return (
-    <div>
-      footer
+
       <div className="bg-white mt-10">
         <div className="container mx-auto">
           <div className="grid grid-cols-4 pt-10">
             <section>
               <nav className="grid gap-y-4">
-                <h6 className="text-lg text-primary-brand-color">
+                <h6 className="text-lg text-[#5d3ebc]">
                   Getir'i indirin!
                 </h6>
                 <a href="#">
-                  <img src="https://getir.com/_next/static/images/appstore-tr-141ed939fceebdcee96af608fa293b31.svg" />
+                  <img className="transitions-all transform hover:scale-105 " src="https://getir.com/_next/static/images/appstore-tr-141ed939fceebdcee96af608fa293b31.svg" />
                 </a>
                 <a href="#">
-                  <img src="https://getir.com/_next/static/images/googleplay-tr-6b0c941b7d1a65d781fb4b644498be75.svg" />
+                  <img className="transitions-all transform hover:scale-105" src="https://getir.com/_next/static/images/googleplay-tr-6b0c941b7d1a65d781fb4b644498be75.svg" />
                 </a>
                 <a href="#">
-                  <img src="https://getir.com/_next/static/images/huawei-appgallery-tr-4b890fa3167bc62f9069edaf45aa7f30.svg" />
+                  <img className="transitions-all transform hover:scale-105" src="https://getir.com/_next/static/images/huawei-appgallery-tr-4b890fa3167bc62f9069edaf45aa7f30.svg" />
                 </a>
               </nav>
             </section>
@@ -132,6 +131,5 @@ export default function Footer() {
           </div>
         </div>
       </div>
-    </div>
   );
 }
